f1afb1c04575e3e9d0b95b6fe37562099e30b303
Matt Hurst <r.matthewhurst@gmail.com>
2026-03-23 20:42:02 -0400

    Project Part 8

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Exercises from './pages/Exercises';
+import Nutrition from './pages/Nutrition';
+import Tutorials from './pages/Tutorials';
+import CaseStudies from './pages/CaseStudies';
 
 function App() {
-  return( 
-  <div>
-    <h1>Hello World</h1>
-    <p>Div Change</p>
-  </div>);
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/exercises" element={<Exercises />} />
+      <Route path="/nutrition" element={<Nutrition />} />
+      <Route path="/tutorials" element={<Tutorials />} />
+      <Route path="/case-studies" element={<CaseStudies />} />
+    </Routes>
+  );
 }
 
 export default App;
